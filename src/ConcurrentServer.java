7ec0bb6d61936adf4d0bfaec0cc55ad04a047c42
ty990520 <ty990520@naver.com>
2022-05-31 13:31:27 +0900

    complete sign up

--- src/ConcurrentServer.java
+++ src/ConcurrentServer.java
@@ -23,6 +23,7 @@ public class ConcurrentServer {
     static ExecutorService executorService; // 스레드풀
     static ServerSocket serverSocket;
     static List<Client> connections = new Vector<Client>();
+    static List<User> loginUsers = new Vector<User>();
 
 
     static void startServer() { // 서버 시작 시 호출
@@ -119,28 +120,17 @@ public class ConcurrentServer {
                 public void run() {
                     try {
                         while (true) {
-//                            byte[] byteArr = new byte[100];
-//                            InputStream inputStream = socket.getInputStream();
-//
-//                            // 데이터 read
-//                            int readByteCount = inputStream.read(byteArr);
-//
-//                            // 클라이언트가 정상적으로 Socket의 close()를 호출했을 경우
-//                            if (readByteCount == -1) {
-//                                throw new IOException();
-//                            }
                             InputStream is = socket.getInputStream();
-                            InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+                            InputStreamReader isr = new InputStreamReader(is, "euc-kr");
                             BufferedReader br = new BufferedReader(isr);
                             // outputStream 가져와서 StreamWriter, PrintWriter로 감싼다
                             OutputStream os = socket.getOutputStream();
-                            OutputStreamWriter osw = new OutputStreamWriter(os, "UTF-8");
+                            OutputStreamWriter osw = new OutputStreamWriter(os, "euc-kr");
                             PrintWriter pw = new PrintWriter(osw, true);
                             // String intro = "\n : 줄 띄어쓰기 \n 안녕하세요";
                             String buffer = null;
                             int temp;
 
-                            String selstore;
                             buffer = br.readLine();
 
                             if (buffer == null) {
@@ -150,18 +140,32 @@ public class ConcurrentServer {
 
                             System.out.println("[요청 처리: " + socket.getRemoteSocketAddress() + ": " + Thread.currentThread().getName() + "]");
 
-                            // 문자열로 변환
-
                             //----------------------------------
                             System.out.println("[server] recieved : " + buffer);
 
 
                             //----------------------------------
                             if (buffer.equals("1")) {
-                                System.out.println("메뉴 1번을 눌렀습니다.");
+                                pw.println("---------로그인---------");
                             } else if (buffer.equals("2")) {
-                                System.out.println("메뉴 2번을 눌렀습니다.");
-
+                                pw.println("---------회원가입---------\n등록할 사용자 아이디를 입력해주세요. >> ");
+                                String userid = userInput(br);
+                                if (userid == null) break;
+                                pw.println("등록할 사용자 패스워드를 입력해주세요. >> ");
+                                String userpw = userInput(br);
+                                if (userpw == null) break;
+                                pw.println("등록할 사용자의 나이를 입력해주세요. >> ");
+                                String age = userInput(br);
+                                if (age == null) break;
+                                pw.println("등록할 사용자 전화번호를 입력해주세요. >> ");
+                                String phone = userInput(br);
+                                if (phone == null) break;
+                                DBDriver dbDriver = new DBDriver();
+                                int result = dbDriver.DBInsert(userid, userpw, age, phone);
+                                if (result != 0) {
+                                    pw.println("회원가입을 완료하였습니다!");
+                                    System.out.println(userid+"님 회원가입 완료");
+                                } else pw.println("회원가입에 실패하였습니다. 다시 시도해주세요.");
                             } else if (buffer.equals("3")) {
                                 System.out.println("메뉴 3번을 눌렀습니다.");
 
@@ -187,6 +191,15 @@ public class ConcurrentServer {
                         }
                     }
                 }
+
+                private String userInput(BufferedReader br) throws IOException {
+                    String userdata = br.readLine();
+                    if (userdata == null) {
+                        System.out.println("회원가입을 취소합니다.");
+                        return null;
+                    }
+                    return userdata;
+                }
             };
             // 스레드풀에서 처리
             executorService.submit(runnable);
@@ -199,7 +212,7 @@ public class ConcurrentServer {
                 public void run() {
                     try {
                         // 클라이언트로 데이터 보내기
-                        byte[] byteArr = data.getBytes("UTF-8");
+                        byte[] byteArr = data.getBytes("euc-kr");
                         OutputStream outputStream = socket.getOutputStream();
                         // 데이터 write
                         outputStream.write(byteArr);
@@ -219,9 +232,9 @@ public class ConcurrentServer {
         }
     }
 
-    public class DBDriver {
-        private final static String URL = "jdbc:mysql://127.0.0.1:3306/jdbc?serverTimezone=Asia/Seoul&useSSL=false";
-        private final static String USER = "root";
+    public static class DBDriver {
+        private final static String URL = "jdbc:mysql://192.168.205.63:3306/jdbc?serverTimezone=Asia/Seoul&useSSL=false";
+        private final static String USER = "newuser";
         private final static String PASSWORD = "1234";
 
         public DBDriver() {
@@ -282,9 +295,11 @@ public class ConcurrentServer {
             }
         }
 
-        void DBInsert() {
+        public int DBInsert(String userid, String userpw, String age, String phone) {
             Connection conn = null; // DB와 연결하기 위한 객체
             PreparedStatement pstmt = null; // SQL 문을 데이터베이스에 보내기위한 객체
+            int result = 0;
+
             try { //Reflection 방식
                 conn = DriverManager.getConnection(URL, USER, PASSWORD); // Connection 생성
 
@@ -292,12 +307,12 @@ public class ConcurrentServer {
                 sql = "insert into user(user_id, password, age, user_phone) values(?,?,?,?)";
                 pstmt = conn.prepareStatement(sql); // PreParedStatement 객체 생성, 객체 생성시 SQL 문장 저장
 
-                pstmt.setString(1, "user123");
-                pstmt.setString(2, "5678");
-                pstmt.setInt(3, Integer.parseInt("24"));
-                pstmt.setString(4, "010-1234-9876");
+                pstmt.setString(1, userid);
+                pstmt.setString(2, userpw);
+                pstmt.setInt(3, Integer.parseInt(age));
+                pstmt.setString(4, phone);
 
-                int r = pstmt.executeUpdate(); // SQL 문장을 실행하고 결과를 리턴 (SQL 문장 실행 후, 변경된 row 수 int type 리턴)
+                result = pstmt.executeUpdate(); // SQL 문장을 실행하고 결과를 리턴 (SQL 문장 실행 후, 변경된 row 수 int type 리턴)
 
             } catch (SQLException e) {
                 System.out.println("[SQL Error : " + e.getMessage() + "]");
@@ -305,6 +320,7 @@ public class ConcurrentServer {
                 // 사용순서와 반대로 close 함
                 pstmtAndConnClose(conn, pstmt);
             }
+            return result;
         }
 
         private void pstmtAndConnClose(Connection conn, PreparedStatement pstmt) {
